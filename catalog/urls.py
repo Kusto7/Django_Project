09fa1ed6056642a952e3762ca0471dbd57641c2f
Kusto7 <kirill.kustov21@gmail.com>
2023-07-06 22:15:33 +0300

    Пробую использовать сайт из домашки 19.1. Пока бьёт ошибками..

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, orders, categories
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('categories/', categories, name='categories'),
+    path('orders/', orders, name='orders'),
+    path('contacts/', contacts, name='contacts')
 ]
