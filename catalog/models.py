543febd1ade89d340e0a38fcc1851a049f6a48f7
Kusto7 <kirill.kustov21@gmail.com>
2023-07-12 00:18:54 +0300

    Готова домашка 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='Название категории')
+    category_description = models.CharField(max_length=150, verbose_name='Описание категории')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.category_name} {self.category_description}'
+
+    class Meta:
+        verbose_name = 'Категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Название продукта')
+    product_description = models.CharField(max_length=150, verbose_name='Описание продукта')
+    product_image = models.ImageField(upload_to='product_media/', verbose_name='Превью продукта', **NULLABLE)
+    product_category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    product_price = models.IntegerField(default=0, verbose_name='Цена за покупку')
+    product_date_create = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    product_date_modified = models.DateField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.product_name} {self.product_description}'
+
+    class Meta:
+        verbose_name = 'Продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Продукты'  # Настройка для наименования набора объектов
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+password = os.getenv('Pass_Postgres')  # Пароль из параметров среды ПК
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Django_project',  # Название БД
+        'USER': 'postgres',  # Пользователь для подключения
+        'PASSWORD': password  # Пароль для этого пользователя # Адрес, на котором развернут сервер БД
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -123,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
