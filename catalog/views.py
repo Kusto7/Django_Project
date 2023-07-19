febf3d70147684edfe8a3b62e8e2e5b9f3cc37a4
Kusto7 <kirill.kustov21@gmail.com>
2023-07-20 02:52:35 +0300

    Домашка 21.1 готова.

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 from catalog.models import Product
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
 
 
 def contacts(request):
